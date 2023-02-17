76d013b1693eb87bb24a3ad72f9feb73fd60962c
pavan <pavankumar.yalamanchili24072001@gmail.com>
2023-02-17 10:53:01 +0530

    commit all

--- demo.js
+++ demo.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const cheerio = require("cheerio");
 const axios = require("axios");
-const xlsx = require("node-xlsx");
-
-const url = "https://www.codechef.com/users/waatstat";
+const xlsx = require("xlsx");
+const url = "https://www.codechef.com/users/";
+const users = [];
 const problems =
   "body > main > div > div > div > div > div > section:nth-child(7) > div";
 const global_rank =
@@ -16,15 +16,40 @@ const division =
   "body > main > div > div > div > aside > div:nth-child(1) > div > div.rating-header.text-center > div:nth-child(2)";
 const stars =
   "body > main > div > div > div > div > div > section.user-details > ul > li:nth-child(1) > span > span.rating";
+let users_completed = 0;
+function read_from_excel() {
+  const file = xlsx.readFile("./Codechef Usernames.xlsx");
+  const sheets = file.SheetNames;
+  const temp = xlsx.utils.sheet_to_json(file.Sheets[file.SheetNames[0]]);
+  temp.forEach((res) => {
+    res.stars = "0★";
+    res.global_rank = 0;
+    res.country_rank = 0;
+    res.rating = 0;
+    res.division = "Div 4";
+    res.problems_solved = 0;
+    res.events_participated = [];
+    res.events_participated_count = 0;
+    users.push(res);
+  });
+  updatedata();
+}
 
-async function getdata() {
-  const { data } = await axios.get(url);
+function write_to_excel() {
+  const workbook = xlsx.utils.book_new();
+  const worksheet = xlsx.utils.json_to_sheet(users);
+  xlsx.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+  xlsx.writeFile(workbook, "data.xlsx");
+}
+async function getdata(user) {
+  users_completed++;
+  const { data } = await axios.get(url + user["New CC Username"]);
   const $ = cheerio.load(data);
-  console.log($(stars).text());
-  console.log(keepnumber($(rating).text()));
-  console.log(removeparantesis($(division).text()));
-  console.log($(global_rank).text());
-  console.log($(country_rank).text());
+  user.stars = $(stars).text();
+  user.rating = keepnumber($(rating).text());
+  user.division = removeparantesis($(division).text());
+  user.global_rank = $(global_rank).text();
+  user.country_rank = $(country_rank).text();
   let problems_solved = {};
   const problems_count = [];
   let article = $(problems).find("article");
@@ -48,13 +73,37 @@ async function getdata() {
       }
     });
   }
-  console.log(problems_solved_count);
-  console.log(events_participated.size);
+
+  user.problems_solved = problems_solved_count;
+  user.events_participated_count = events_participated.size;
+  user.events_participated = Array.from(events_participated).toString();
 }
 
-getdata().then(() => {
-  // console.log("done");
-});
+function updatedata() {
+  let count = 0;
+  for (let i = 0; i < users.length; i++) {
+    getdata(users[i])
+      .then(() => {
+        count += 1;
+        console.clear();
+        console.log(count);
+      })
+      .catch((err) => {
+        count += 1;
+        console.clear();
+        console.log(count);
+      })
+      .finally(() => {
+        write_to_excel();
+      });
+  }
+}
+
+read_from_excel();
+
+// getdata().then(() => {
+//   // console.log("done");
+// });
 
 function keepnumber(str) {
   let return_value = "";
---
+++ dym.js
@@ -0,0 +1,5 @@
+let a = new Set();
+a.add(1);
+a.add(2);
+a.add(3);
+console.log(Array.from(a).toString());
